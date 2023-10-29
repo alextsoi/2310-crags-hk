2869cec2bd40c273cb29f6a11dfea5ed1d36a3a7
Alex Tsoi <alex@k2.digital>
2023-10-30 00:52:32 +0800

    updated

--- app/page.js
+++ app/page.js
@@ -1,14 +1,26 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
 import routes from './data/routes.json'
 import blocs from './data/blocs.json'
 
 export default function Home() {
+  const ratingText = ['', '*', '**', '***']
   return (
     <main className={styles.main}>
       <h1>Problems</h1>
       {blocs.data.map((bloc) => {
-        return <h2>{bloc.id} - {bloc.name}</h2>;
+        return <section className={styles.bloc}>
+          <h2>{bloc.id} - {bloc.name}</h2>
+          <ul className={styles.blocRoutes}>
+            {routes.data.filter(route => route.blocs === bloc.id).map((route) => {
+              return <li className={styles.blocRoute} key={route.id}>
+                <div className={styles.blocRouteGrade}>{route.gradings.map(grade => grade !== 'project' ? `V${grade}` : grade).join('/')}</div>
+                <div className={styles.blocRouteId}>{route.id} - </div>
+                <div className={styles.blocRouteName}>{route.name}{route.isSds ? ' (sds)' : ''}{route.rating !== 0 ? ratingText[route.rating] : ''}</div>
+              </li>
+            })}
+          </ul>
+        </section>;
       })}
     </main>
   )
