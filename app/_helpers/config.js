4ad19f35d4d8ad787e404fe9f801e4dc84b0a1f0
Alex Tsoi <alex@k2.digital>
2023-10-30 12:39:08 +0800

    adding landing SEO

--- app/_helpers/config.js
+++ app/_helpers/config.js
@@ -1,2 +1,4 @@
 export const ratingText = ['', '*', '**', '***'];
 export const websiteHost = 'https://crags.hk/';
+export const gtmCode = "GTM-K6NB4GWK";
+export const siteName = 'CRAGS.HK - Sunset Forest';
--- app/layout.js
+++ app/layout.js
@@ -2,17 +2,42 @@ import { Inter } from 'next/font/google'
 import './globals.scss'
 import Script from 'next/script'
 import Link from 'next/link'
+import { gtmCode, siteName, websiteHost } from './_helpers/config'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'CRAGS.HK - Hong Kong Online Bouldering Guidebook',
   description: 'Explore Sunset Forest, one of the premier bouldering and climbing sites in Hong Kong, with ease. Our comprehensive online guidebook provides detailed guides and information based on blocs, grades, and ratings, segmented by specific zones. With minimal data usage and fast browsing speed, you can get the latest updates and plan your next adventure effortlessly, even in areas with poor signal or when low on data. Discover the joy of bouldering and climbing with us - your ultimate guide to Sunset Forest, designed for easy access anytime, anywhere.',
+  openGraph: {
+    title: siteName,
+    description: 'Explore Sunset Forest, one of the premier bouldering and climbing sites in Hong Kong, with ease. Our comprehensive online guidebook provides detailed guides and information based on blocs, grades, and ratings, segmented by specific zones. With minimal data usage and fast browsing speed, you can get the latest updates and plan your next adventure effortlessly, even in areas with poor signal or when low on data. Discover the joy of bouldering and climbing with us - your ultimate guide to Sunset Forest, designed for easy access anytime, anywhere.',
+    url: websiteHost,
+    siteName: siteName,
+    images: [
+      {
+        url: `${websiteHost}og-image.jpg`,
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${gtmCode}');
+        `}
+      </Script>
       <body className={inter.className}>
         <header className="root-header">
           <div className="container flex-container">
@@ -40,6 +65,11 @@ export default function RootLayout({ children }) {
             <br />- By alex
           </div>
         </footer>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmCode}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   )
