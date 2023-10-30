8a3bc8ce21a9581be5e832f84faf06432bae7bb1
Alex Tsoi <alex@k2.digital>
2023-10-30 12:56:11 +0800

    favicon

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import './globals.scss'
 import Script from 'next/script'
 import Link from 'next/link'
 import { gtmCode, siteName, websiteHost } from './_helpers/config'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,6 +39,15 @@ export default function RootLayout({ children }) {
         })(window,document,'script','dataLayer','${gtmCode}');
         `}
       </Script>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className={inter.className}>
         <header className="root-header">
           <div className="container flex-container">
