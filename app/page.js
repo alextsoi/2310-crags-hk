5f5d25325765201cbc3f51c1821b363b6f1ce29d
Alex Tsoi <alex@k2.digital>
2023-10-31 12:32:16 +0800

    updated the zone images

--- app/page.js
+++ app/page.js
@@ -11,8 +11,8 @@ export default function Home() {
                 <p>Currently, our focus is solely on providing the most accurate, up-to-date information for the Sunset Forest bouldering site. We are committed to making your experience in Sunset Forest as enjoyable and fulfilling as possible, helping you discover the joy of bouldering and climbing in this remarkable location.</p>
                 <p>While Sunset Forest is our current area of focus, we are continually looking to expand and improve. We're excited to share that we have new features in the pipeline, designed to enhance your bouldering and climbing experience even further. Stay tuned for updates coming soon!</p>
                 <p>Join us at CRAGS.HK, your trusted companion in the world of bouldering and climbing, as we guide you through the wonders of Sunset Forest.</p>
-                <h3>Access</h3>
-
+                <h3><a href="https://maps.app.goo.gl/imrdXQzcdumx23VM6" target="_blank" title="Access & Location in Google Map | Complete Guide to Bouldering and Climbing in Sunset Forest | CRAGS.HK">Access</a></h3>
+<p>We already created a Google marker of Sunset Forest, please click <a href="https://maps.app.goo.gl/imrdXQzcdumx23VM6" target="_blank" title="Access & Location in Google Map | Complete Guide to Bouldering and Climbing in Sunset Forest | CRAGS.HK">here</a> to check the location</p>
             </div>
         </main>
     )
