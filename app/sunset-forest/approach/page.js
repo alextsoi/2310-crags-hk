0e09b929ecd5317e0156fc4e7650f219edc73524
Alex Tsoi <alex@k2.digital>
2024-03-11 16:13:59 +0800

    updated the approach page to full CMS support

--- app/sunset-forest/approach/page.js
+++ app/sunset-forest/approach/page.js
@@ -35,50 +35,30 @@ export default async function Approach() {
     const file = path.join(process.cwd(), filename);
     const fileContent = await fs.readFile(file, 'utf8');
     const data = matter(fileContent).data;
+    let images = data.images;
     let content = data.content;
     let processor = unified().use(remarkParse).use(html);
     let contentResult = await processor.process(content);
-    console.log(contentResult);
     contentResult = contentResult.toString();
     let content2 = data.content2;
-    let content2Result = '';
-    try {
-        console.log(content2);
-        content2Result = await processor.process(content2);
-        console.log(content2Result);
-        content2Result = content2Result.toString();
-    } catch (e) {
-        console.log(e);
-    }
+    let content2Result = await processor.process(content2);
+    content2Result = content2Result.toString();
     return (
         <main className={styles.main}>
             <div className="container">
                 <h1>Sunset Forest Approach</h1>
                 <div dangerouslySetInnerHTML={{ __html: contentResult }}></div>
                 <div className={styles.approachWrapper}>
-                    <div className={styles.approach}>
-                        <img loading="lazy" width={396} height={704} src="/images/common/approach-1.jpg" alt="Sunset Forest Bouldering Site Approach Step 1 | CRAGS.HK" />
-                    </div>
-                    <div className={styles.approach}>
-                        <img loading="lazy" width={396} height={704} src="/images/common/approach-2.jpg" alt="Sunset Forest Bouldering Site Approach Step 2 | CRAGS.HK" />
-                    </div>
-                    <div className={styles.approach}>
-                        <img loading="lazy" width={396} height={704} src="/images/common/approach-3.jpg" alt="Sunset Forest Bouldering Site Approach Step 3 | CRAGS.HK" />
-                    </div>
-                    <div className={styles.approach}>
-                        <img loading="lazy" width={396} height={704} src="/images/common/approach-4.jpg" alt="Sunset Forest Bouldering Site Approach Step 4 | CRAGS.HK" />
-                    </div>
-                    <div className={styles.approach}>
-                        <img loading="lazy" width={396} height={704} src="/images/common/approach-5.jpg" alt="Sunset Forest Bouldering Site Approach Step 5 | CRAGS.HK" />
-                    </div>
-                    <div className={styles.approach}>
-                        <img loading="lazy" width={396} height={704} src="/images/common/approach-6.jpg" alt="Sunset Forest Bouldering Site Approach Step 6 | CRAGS.HK" />
-                    </div>
-                    <div className={styles.approach}>
-                        <img loading="lazy" width={396} height={704} src="/images/common/approach-7.jpg" alt="Sunset Forest Bouldering Site Approach Step 7 | CRAGS.HK" />
-                    </div>
+                    {
+                        images && images.map((image, index) => {
+                            return (
+                                <div key={index} className={styles.approach}>
+                                    <img loading="lazy" width={396} height={704} src={image} alt={`Sunset Forest Bouldering Site Approach Step ${index + 1} | CRAGS.HK`} />
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-                
                 <div dangerouslySetInnerHTML={{ __html: content2Result }}></div>
             </div>
         </main>
